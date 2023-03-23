56b243e8f3b24a64f0a41398d2b7826921f925d9
Deep Awasthi <da.madskull@gmail.com>
2023-03-23 10:08:08 +0530

    Initial Commit

---
+++ src/main/java/com/deepawasthi/SpringPaypalPayment/SpringPaypalPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.deepawasthi.SpringPaypalPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaypalPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepawasthi/SpringPaypalPayment/SpringPaypalPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepawasthi.SpringPaypalPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaypalPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
